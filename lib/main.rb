53c17e94c02973815d70f2b6267bc6a85c1b9b24
jeffbbz <tfpaco@gmail.com>
2025-05-30 11:24:30 -0400

    refactor: Encapsulate HTML to JSON processing in a dedicated class

--- lib/main.rb
+++ lib/main.rb
@@ -3,35 +3,33 @@
 require 'json'
 require_relative 'parser'
 
-def load_html_files
-  Dir.glob(File.join(__dir__, '..', 'files', '*.html'))
-end
+class HtmlToJsonFileProcessor
+  def initialize
+    @parser = GoogleSearch::Parser.new
+  end
 
-def process_html_to_json
-  load_html_files.each do |file|
-    parser = GoogleSearch::Parser.new
-    parsed = parser.parse_html_file(file)
-    output_to_json(file, parsed)
+  def process_html_to_json
+    load_html_files.each do |file|
+      parsed = @parser.parse_html_file(file)
+      output_to_json(file, parsed)
+    end
+  end
+
+  private
+
+  def load_html_files
+    Dir.glob(File.join(__dir__, '..', 'files', '*.html'))
+  end
+
+  def output_to_json(file, parsed)
+    output_directory = File.join(__dir__, '..', 'output')
+    Dir.mkdir(output_directory) unless Dir.exist?(output_directory)
+
+    output_file_name = "#{File.basename(file, '.html')}.json"
+    output_file = File.join(output_directory, output_file_name)
+
+    File.write(output_file, JSON.pretty_generate(parsed))
   end
 end
 
-def output_to_json(file, parsed)
-  output_directory = File.join(__dir__, '..', 'output')
-  Dir.mkdir(output_directory) unless Dir.exist?(output_directory)
-
-  output_file_name = "#{File.basename(file, '.html')}.json"
-  output_file = File.join(output_directory, output_file_name)
-
-  File.write(output_file, JSON.pretty_generate(parsed))
-end
-
-process_html_to_json
-
-# class Greetings
-#   def self.hello_world
-#     'Hello, world!'
-#   end
-# end
-
-# puts Greetings.hello_world
-
+HtmlToJsonFileProcessor.new.process_html_to_json
